0190fbbd17149511a457543ba0a83b775fec75a2
Narawit Triprom <61010556@kmitl.ac.th>
2021-08-09 11:31:06 +0700

    add daisyui

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-	<meta name="description" content="Beryl Hardware Labolatory platform">
+	<meta name="description" content="Beryl Hardware Labolatory platform" />
 </svelte:head>
 <div class="py-12 bg-white">
 	<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -17,8 +17,8 @@
 			<dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
 				<div class="relative">
 					<dt>
-						<div
-							class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white"
+						<button
+							class="absolute btn btn-primary flex items-center justify-center h-12 w-12 p-0  text-white"
 						>
 							<!-- Heroicon name: outline/globe-alt -->
 							<svg
@@ -36,7 +36,7 @@
 									d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
 								/>
 							</svg>
-						</div>
+						</button>
 						<p class="ml-16 text-lg leading-6 font-medium text-gray-900">No Hardware Require</p>
 					</dt>
 					<dd class="mt-2 ml-16 text-base text-gray-500">
@@ -47,8 +47,8 @@
 
 				<div class="relative">
 					<dt>
-						<div
-							class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white"
+						<button
+							class="absolute btn btn-primary flex items-center justify-center h-12 w-12 p-0  text-white"
 						>
 							<!-- Heroicon name: outline/scale -->
 							<svg
@@ -66,11 +66,11 @@
 									d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
 								/>
 							</svg>
-						</div>
+						</button>
 						<p class="ml-16 text-lg leading-6 font-medium text-gray-900">Less human interactive</p>
 					</dt>
 					<dd class="mt-2 ml-16 text-base text-gray-500">
-                        แพลตฟอร์มรองรับการทดสอบและออกรายงานโดยอัตโนมัติ ลดการใช้แรงงานในการตรวจสอบโดยใช้มนุษย์
+						แพลตฟอร์มรองรับการทดสอบและออกรายงานโดยอัตโนมัติ ลดการใช้แรงงานในการตรวจสอบโดยใช้มนุษย์
 					</dd>
 				</div>
 			</dl>
