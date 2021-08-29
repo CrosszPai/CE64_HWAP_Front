3c135f0e9b231f30bba63504e6d769c03c1509e0
AlphabetJ <Jate44183@gmail.com>
2021-08-29 20:02:20 +0700

    เพิ่มหัวข้อของแต่ละเมนู

---
+++ src/routes/summary.svelte
@@ -0,0 +1,15 @@
+<div class="card shadow-lg w-300px">
+	<div class="card-body">
+		<h2 class="card-title w-300px">Lastest Result</h2>
+	</div>
+</div>
+<div class="card shadow-lg ">
+	<div class="card-body">
+		<h2 class="card-title w-300px">Countinue Lab</h2>
+	</div>
+</div>
+<div class="card shadow-lg w-300px">
+	<div class="card-body">
+		<h2 class="card-title w-300px">Explore Another Lab</h2>
+	</div>
+</div>
