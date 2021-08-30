cd2d05e6ba5b5954211f0c012fb72520fe7861b9
AlphabetJ <Jate44183@gmail.com>
2021-08-30 15:55:58 +0700

    เปลี่ยนขนาดของกรอบในแต่ละเมนู

--- src/routes/summary.svelte
+++ src/routes/summary.svelte
@@ -1,15 +1,17 @@
-<div class="card shadow-lg w-300px">
-	<div class="card-body">
-		<h2 class="card-title w-300px">Lastest Result</h2>
+<div class="flex md:items-center">
+	<div class="card shadow-lg flex lg:card-side mx-10 w-96">
+		<div class="card-body">
+			<h2 class="card-title">Lastest Result</h2>
+		</div>
 	</div>
-</div>
-<div class="card shadow-lg ">
-	<div class="card-body">
-		<h2 class="card-title w-300px">Countinue Lab</h2>
+	<div class="card shadow-lg flex lg:card-side mx-10 w-96">
+		<div class="card-body">
+			<h2 class="card-title">Countinue Lab</h2>
+		</div>
 	</div>
-</div>
-<div class="card shadow-lg w-300px">
-	<div class="card-body">
-		<h2 class="card-title w-300px">Explore Another Lab</h2>
+	<div class="card shadow-lg flex lg:card-side mx-10 w-96">
+		<div class="card-body">
+			<h2 class="card-title">Explore Another Lab</h2>
+		</div>
 	</div>
 </div>
