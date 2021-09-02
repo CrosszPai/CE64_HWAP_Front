9b9caac1a8392ec778dfeb4f892b7ba6c35550a6
AlphabetJ <Jate44183@gmail.com>
2021-09-02 18:26:43 +0700

    เพิ่มปุ่ม แต่ยังจัดให้อยู่ตรงกลางไม่ได้

--- src/routes/summary.svelte
+++ src/routes/summary.svelte
@@ -1,17 +1,20 @@
 <div class="flex md:items-center">
 	<div class="card shadow-lg flex lg:card-side mx-10 w-96">
-		<div class="card-body">
-			<h2 class="card-title">Lastest Result</h2>
+		<div class="card-body flex flex-col justify-center">
+			<h2 class="card-title items-center">Lastest Result</h2>
+			<button class="btn btn-outline pt-8">More</button>
 		</div>
 	</div>
 	<div class="card shadow-lg flex lg:card-side mx-10 w-96">
-		<div class="card-body">
+		<div class="card-body flex flex-col  place-items-center">
 			<h2 class="card-title">Countinue Lab</h2>
+			<button class="btn btn-outline pt-8">More</button>
 		</div>
 	</div>
 	<div class="card shadow-lg flex lg:card-side mx-10 w-96">
-		<div class="card-body">
+		<div class="card-body flex flex-col  place-items-center">
 			<h2 class="card-title">Explore Another Lab</h2>
+			<button class="btn btn-outline pt-8">More</button>
 		</div>
 	</div>
 </div>
