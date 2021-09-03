1e1ebc790aaa862896d24ecc4f8366446936e2c2
AlphabetJ <Jate44183@gmail.com>
2021-09-03 16:12:16 +0700

    จัด css

--- src/routes/summary.svelte
+++ src/routes/summary.svelte
@@ -1,20 +1,26 @@
-<div class="flex md:items-center">
-	<div class="card shadow-lg flex lg:card-side mx-10 w-96">
-		<div class="card-body flex flex-col justify-center">
-			<h2 class="card-title items-center">Lastest Result</h2>
-			<button class="btn btn-outline pt-8">More</button>
+<div class="flex md:items-center w-full">
+	<div class="text-center">
+		<div class="card shadow-lg flex lg:card-side mx-10 w-96">
+			<div class="card-body flex flex-col  place-items-center">
+				<h2 class="card-title">Lastest Result</h2>
+			</div>
 		</div>
+		<button class="btn btn-outline mt-8">More</button>
 	</div>
-	<div class="card shadow-lg flex lg:card-side mx-10 w-96">
-		<div class="card-body flex flex-col  place-items-center">
-			<h2 class="card-title">Countinue Lab</h2>
-			<button class="btn btn-outline pt-8">More</button>
+	<div class="text-center">
+		<div class="card shadow-lg flex lg:card-side mx-10 w-96">
+			<div class="card-body flex flex-col  place-items-center">
+				<h2 class="card-title">Continue Lab</h2>
+			</div>
 		</div>
+		<button class="btn btn-outline mt-8">More</button>
 	</div>
-	<div class="card shadow-lg flex lg:card-side mx-10 w-96">
-		<div class="card-body flex flex-col  place-items-center">
-			<h2 class="card-title">Explore Another Lab</h2>
-			<button class="btn btn-outline pt-8">More</button>
+	<div class="text-center">
+		<div class="card shadow-lg flex lg:card-side mx-10 w-96">
+			<div class="card-body flex flex-col  place-items-center">
+				<h2 class="card-title">Explore Another Lab</h2>
+			</div>
 		</div>
+		<button class="btn btn-outline mt-8">More</button>
 	</div>
 </div>
