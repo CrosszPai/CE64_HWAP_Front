d3192300a18f9a3f9daa205c2dfd80bf06ea62f6
AlphabetJ <Jate44183@gmail.com>
2021-09-08 14:24:26 +0700

    เพิ่มหน้า form สำหรับใส่ description lab

--- src/routes/instructorlab.svelte
+++ src/routes/instructorlab.svelte
@@ -1,1 +1,15 @@
-<div>This is Instructor Lab</div>
+<div>
+	<div class="p-10 card bg-base-200">
+		<div class="form-control mb-5">
+			<label class="label text-4xl">
+				<span class="label-text text-4xl">Lab 1 : Lab Name</span>
+			</label>
+			<textarea class="textarea h-24" placeholder="details" />
+		</div>
+		<button
+			class="btn btn-primary btn-active self-end btn"
+			role="button"
+			aria-pressed="true">Submit</button
+		>
+	</div>
+</div>
