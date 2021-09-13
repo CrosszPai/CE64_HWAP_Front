64a7796da49b1cdd693cfe206ebbe1ef1fc7a2af
Narawit Triprom <61010556@kmitl.ac.th>
2021-09-14 01:31:24 +0700

    init storybook

---
+++ src/stories/Test.stories.svelte
@@ -0,0 +1,27 @@
+<script>
+import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+
+import Summary from "../routes/summary.svelte";
+import '../app.postcss';
+</script>
+
+<Meta
+  title="Summary/Sum"
+  component={Summary}
+/>
+
+<Template let:args>
+
+<div class="p-2 container mx-auto">
+    <Summary
+    {...args}
+  />
+</div>
+    
+  </Template>
+  
+<Story
+  name="LoggedIn"
+  args={{
+  }}
+/>
