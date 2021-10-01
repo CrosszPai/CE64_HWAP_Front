18003b79c9f0ca972dad8d1ee83b484e4dde12fa
AlphabetJ <Jate44183@gmail.com>
2021-10-01 10:30:12 +0700

    ทำหน้า lab ให้แสดง test case

--- src/routes/studentlab.svelte
+++ src/routes/studentlab.svelte
@@ -17,14 +17,25 @@
 	}
 </script>
 
-<div class="text-center w-full">
-	<div class="p-10 card bg-base-200 h-1/2">
-		<form class="form-control mb-5 mt-5" on:submit|preventDefault={submit_lab}>
-			<textarea class="textarea h-2/4 mt-5" placeholder="lab name" bind:value={lab_name} />
-			<textarea class="textarea h-2/4 mt-5" placeholder="details" bind:value={lab_detail} />
-			<button class="btn btn-primary btn-active self-end btn mt-5" role="button" aria-pressed="true"
-				>Submit</button
-			>
-		</form>
+<div class="card lg:card-side bordered shadow-lg flex">
+	<div class="card-body flex flex-col">
+		<h2 class="card-title text-4xl mt-5 text-purple-600">lab name</h2>
+		<p class="ml-10 mt-5">lab details</p>
 	</div>
 </div>
+<div class="card-body flex flex-col shadow-lg ">
+	<div class="flex flex-col">
+		<div class="justify-items-start">
+			<input type="checkbox" checked="checked" class="checkbox" /> test case 1
+		</div>
+		<div class="justify-items-start">
+			<input type="checkbox" checked="checked" class="checkbox" /> test case 2
+		</div>
+		<div class="justify-items-start">
+			<input type="checkbox" checked="checked" class="checkbox" /> test case 3
+		</div>
+	</div>
+</div>
+<button class="btn btn-primary btn-active self-end btn mt-5" role="button" aria-pressed="true"
+	>Submit</button
+>
