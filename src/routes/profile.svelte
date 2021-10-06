c7a9c942ad0105c6cf956a8fac7aea03afe8678f
AlphabetJ <Jate44183@gmail.com>
2021-10-06 10:28:24 +0700

    connect profile to api

--- src/routes/profile.svelte
+++ src/routes/profile.svelte
@@ -1,13 +1,17 @@
+<script>
+	import { session } from '$app/stores';
+
+	console.log($session.user);
+</script>
+
 <div class="card lg:card-side bordered shadow-lg mt-5">
 	<figure>
-		<img
-			src="https://i.stack.imgur.com/UM6Nt.png"
-		/>
+		<img src={$session.user.avatar_url} class="w-[200px] h-[200px]" alt="profile" width="200" height="200"/>
 	</figure>
 	<div class="card-body mt-3">
-		<h2 class="card-title">Name : Yanothai Apanutikul</h2>
+		<h2 class="card-title">Name : {$session.user.name}</h2>
 		<h2 class="card-title">ID : 61010260</h2>
-		<h2 class="card-title">Email : 61010260@kmitl.ac.th</h2>
+		<h2 class="card-title">Email : {$session.user.email}</h2>
 	</div>
 </div>
 
