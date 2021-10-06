ecb185351f4b92ee5aa9e4b53f605d303a162eff
AlphabetJ <Jate44183@gmail.com>
2021-10-06 11:10:00 +0700

    edit result page

--- src/routes/result.svelte
+++ src/routes/result.svelte
@@ -1,37 +1,41 @@
+<script>
+	import { session } from '$app/stores';
+
+	console.log($session.user);
+</script>
+
 <div class="card lg:card-side bordered shadow-lg mt-5">
 	<figure>
 		<img
-			src="https://i.stack.imgur.com/UM6Nt.png"
+			src={$session.user.avatar_url}
+			class="w-[200px] h-[200px]"
+			alt="profile"
+			width="200"
+			height="200"
 		/>
 	</figure>
 	<div class="card-body mt-3">
-		<h2 class="card-title">Name : Yanothai Apanutikul</h2>
+		<h2 class="card-title">Name : {$session.user.name}</h2>
 		<h2 class="card-title">ID : 61010260</h2>
-		<h2 class="card-title">Email : 61010260@kmitl.ac.th</h2>
+		<h2 class="card-title">Email : {$session.user.email}</h2>
 	</div>
 </div>
 
-<div class="card lg:card-side bordered shadow-lg mt-5 mb-5 flex">
-	<div class="card-body flex-col">
-		<h2 class="card-title">
-			Lab 1
-			<div class="badge badge-success ml-3">success</div>
-		</h2>
-		<div
-			class="card text-center shadow-2xl lg:card-side bg-accent text-accent-content justify-center flex flex-col"
-		>
-			<div class="card-body flex flex-col">
-				<div class="flex flex-col">
-					<div class="justify-items-start">
-						<input type="checkbox" checked="checked" class="checkbox" /> test case 1
-					</div>
-					<div class="justify-items-start">
-						<input type="checkbox" checked="checked" class="checkbox" /> test case 2
-					</div>
-					<div class="justify-items-start">
-						<input type="checkbox" checked="checked" class="checkbox" /> test case 3
-					</div>
-				</div>
+<div class="flex flex-col items-center">
+	<div class="card lg:card-side bordered shadow-lg mt-5 mb-5 w-1/2">
+		<div class="card-body">
+			<h2 class="card-title">
+				Lab 1
+				<div class="badge badge-success ml-3">success</div>
+			</h2>
+			<div>
+				<input type="checkbox" checked class="checkbox mr-2" /> test case 1
+			</div>
+			<div>
+				<input type="checkbox" checked class="checkbox mr-2" /> test case 2
+			</div>
+			<div>
+				<input type="checkbox" checked class="checkbox mr-2" /> test case 3
 			</div>
 		</div>
 	</div>
