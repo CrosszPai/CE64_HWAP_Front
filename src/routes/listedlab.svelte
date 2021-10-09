05d1599e4bd14c88cd176d2619207460509bbacd
AlphabetJ <Jate44183@gmail.com>
2021-10-09 19:36:58 +0700

    put lab name in the card

--- src/routes/listedlab.svelte
+++ src/routes/listedlab.svelte
@@ -11,11 +11,17 @@
 {:else}
 	<ul>
 		{#each labs.data.selfLabs as lab, i}
-            <li>
+            <!-- <li>
                 {lab.id}
 				{lab.lab_name}
 				{lab.lab_detail}
-            </li>
+            </li> -->
+			<div class="card shadow-lg">
+				<div class="card-body">
+				  <h2 class="card-title">{lab.lab_name}</h2> 
+				  <p>{lab.lab_detail}</p>
+				</div>
+			</div>
         {/each}
 	</ul>
 {/if}
