0497f115c18a873d9a6f135511fffae2aad428a9
AlphabetJ <Jate44183@gmail.com>
2021-10-16 22:06:33 +0700

    edit result page

--- src/routes/result.svelte
+++ src/routes/result.svelte
@@ -4,28 +4,34 @@
 	console.log($session.user);
 </script>
 
-<div class="card lg:card-side bordered shadow-lg mt-5">
-	<figure>
-		<img
-			src={$session.user.avatar_url}
-			class="w-[200px] h-[200px]"
-			alt="profile"
-			width="200"
-			height="200"
-		/>
-	</figure>
-	<div class="card-body mt-3">
-		<h2 class="card-title">Name : {$session.user.name}</h2>
-		<h2 class="card-title">ID : 61010260</h2>
-		<h2 class="card-title">Email : {$session.user.email}</h2>
+<div class="card lg:card-side bordered shadow-lg flex flex-col">
+	<div class="card-body flex flex-col">
+		<h2 class="card-title text-4xl mt-5 text-purple-600">Lab Name</h2>
+		<p class="ml-10 mt-5">Lab Detail</p>
+	</div>
+
+	<div class="card-body flex flex-col">
+		<h2 class="card-title">
+			Test Cases
+			<div class="badge badge-success ml-3">success</div>
+		</h2>
+		<div>
+			<input type="checkbox" checked class="checkbox mr-2" /> test case 1
+		</div>
+		<div>
+			<input type="checkbox" checked class="checkbox mr-2" /> test case 2
+		</div>
+		<div>
+			<input type="checkbox" checked class="checkbox mr-2" /> test case 3
+		</div>
 	</div>
 </div>
 
-<div class="flex flex-col items-center">
+<!-- <div class="flex flex-col items-center">
 	<div class="card lg:card-side bordered shadow-lg mt-5 mb-5 w-1/2">
 		<div class="card-body">
 			<h2 class="card-title">
-				Lab 1
+				Test Cases
 				<div class="badge badge-success ml-3">success</div>
 			</h2>
 			<div>
@@ -39,4 +45,4 @@
 			</div>
 		</div>
 	</div>
-</div>
+</div> -->
