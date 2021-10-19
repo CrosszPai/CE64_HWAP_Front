b299def578956b4b6b975b3acab9e4ca33f42631
AlphabetJ <Jate44183@gmail.com>
2021-10-19 20:00:42 +0700

    ทำหน้า result อีกแบบนึง

---
+++ src/routes/result2.svelte
@@ -0,0 +1,24 @@
+<div class="card lg:card-side bordered shadow-lg">
+    <div class="card-body">
+        <h2 class="card-title text-4xl mt-5 text-purple-600">lab name</h2>
+        <p class="ml-10 mt-5">lab detail</p>
+        <div class="flex flex-col items-center">
+            <div class="card lg:card-side bordered shadow-lg mt-5 mb-5 ">
+                <div class="card-body">
+                    
+                    <div>
+                        <input type="checkbox" checked class="checkbox mr-2" /> test case 1
+                    </div>
+                    <div>
+                        <input type="checkbox" checked class="checkbox mr-2" /> test case 2
+                    </div>
+                    <div>
+                        <input type="checkbox" checked class="checkbox mr-2" /> test case 3
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+</div>
