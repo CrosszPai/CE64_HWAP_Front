a3cd2b521b2556fc42e25aca154bbdf46f5ed4de
AlphabetJ <Jate44183@gmail.com>
2021-10-21 18:02:39 +0700

    edit some flex

--- src/routes/result2.svelte
+++ src/routes/result2.svelte
@@ -1,7 +1,7 @@
 <div class="card lg:card-side bordered shadow-lg">
-    <div class="card-body">
-        <h2 class="card-title text-4xl mt-5 text-purple-600">lab name</h2>
-        <p class="ml-10 mt-5">lab detail</p>
+    <div class="card-body flex">
+        <h2 class="card-title text-4xl mt-5 text-purple-600 flex-col">lab name</h2>
+        <p class="ml-10 mt-5 flex-col">lab detail</p>
         <div class="flex flex-col items-center">
             <div class="card lg:card-side bordered shadow-lg mt-5 mb-5 ">
                 <div class="card-body">
