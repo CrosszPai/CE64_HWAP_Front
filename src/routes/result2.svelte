8b779c6ca4763043176cbe9cffa6a7860986d3e4
AlphabetJ <Jate44183@gmail.com>
2021-10-30 23:13:51 +0700

    edit some css

--- src/routes/result2.svelte
+++ src/routes/result2.svelte
@@ -3,7 +3,7 @@
         <h2 class="card-title text-4xl mt-5 text-purple-600 flex-col">lab name</h2>
         <p class="ml-10 mt-5 flex-col">lab detail</p>
         <div class="flex flex-col items-center">
-            <div class="card lg:card-side bordered shadow-lg mt-5 mb-5 ">
+            <div class="card lg:card-side bordered shadow-lg mt-5 mb-5 w-1/2">
                 <div class="card-body flex flex-col">
                     
                     <div>
